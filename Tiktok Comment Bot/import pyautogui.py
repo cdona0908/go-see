ec10d4eccf247ac8a31289fd39854b8159afd2de
Cyres Cooper <95892453+CyresCooper@users.noreply.github.com>
2021-12-19 11:53:55 -0500

    first commit

---
+++ Tiktok Comment Bot/import pyautogui.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+time.sleep(5)
+print(pyautogui.position())
+
+#For the boys only check my liked videos
+#I dare you to look at my liked videos
+#Trust me boys look at my liked videos
+#Youll never believe whats in my liked videos
+#Dont look at my liked videos
