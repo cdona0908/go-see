790ad0647c61f6c90f1d05ff1fa0401c572ef54c
Ashok Nayak <ashok.nayakii@gmail.com>
2022-02-02 17:45:38 -0500

    success using second fetch forgot return command

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 
 var getMovieInfo = function () {
 
-    var apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=b1ac471e&type=movie&y=2021"
+    var apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=b1ac471e"
+    
+
+    // apikey for youtube : "http://www.youtube.googleapis.com/AIzaSyBC25-sXfyC7zAewvLulX5KxCLKO_nvJX8"
+    
 
 
     // fetch format that my tutor suggested
@@ -12,20 +16,38 @@ var getMovieInfo = function () {
     //         console.log(responseData)
     //         .catch(err => { console.log(err) })
 
+    // });
 
     // fetch format that I got from the module
 
-            // fetch(apiUrl) 
-            //     .then(function (response) {
-            //         return response.json();
-            //     })
-            //     .then(function (data) {
+            fetch(apiUrl) 
+                .then(function (response) {
+                    console.log("response");
+                    return response.json();
+                })
+                .then(function (data) {
+                        console.log("data"); 
+                        console.log(data);           
+                });
+
+                
+
+    // original fetch request tried at the beginning with response but no data
+
+            // fetch(apiUrl)
+            // .then (function (response) {
+            //     if (response.ok) {
+            //         console.log (response);
+
+            //         response.json()
+            //         .then(function (data) {
             //             console.log(data);
-            //     });
+            //         });
+            //     }
+            // })
+    };
 
-
-
-    }
+    getMovieInfo();
 
     
 
