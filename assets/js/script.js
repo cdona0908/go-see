1b428b727e8df007425b9fff50a9166a353b1e4c
Ashok Nayak <ashok.nayakii@gmail.com>
2022-02-02 15:26:45 -0500

    working on bug fetching omdb api

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+
+var getMovieInfo = function () {
+
+    var apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=b1ac471e&type=movie&y=2021"
+
+
+    // fetch format that my tutor suggested
+
+    // fetch(apiUrl)
+    //     .then(function (res) { res.json() })
+    //     .then(function (responseData) {
+    //         console.log(responseData)
+    //         .catch(err => { console.log(err) })
+
+
+    // fetch format that I got from the module
+
+            // fetch(apiUrl) 
+            //     .then(function (response) {
+            //         return response.json();
+            //     })
+            //     .then(function (data) {
+            //             console.log(data);
+            //     });
+
+
+
+    }
+
+    
+
+
